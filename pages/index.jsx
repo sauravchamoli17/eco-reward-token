34e5c067f652a57634e7394d763f474c27f94efd
Saurav Chamoli <sauravchamoli17@gmail.com>
2023-05-16 15:37:09 +0530

    Changed Page Title

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,10 +6,14 @@ import Navbar from "@/components/Navbar";
 import Transactions from "@/components/Transactions";
 import Hero from "@/components/Hero";
 import styles from "@/styles/Home.module.css";
+import Head from 'next/head'; 
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>ECO Reward Token</title>
+      </Head>
       <Navbar />
       <Hero />
       <MintInfo />
